2d9a47efe44a7dfc9114637a21fbb4ec88d6d8f8
Deborah Barnard <deborah@starfallprojects.co.uk>
2021-08-26 12:14:34 +0100

    add delay

--- script.ps1
+++ script.ps1
@@ -1,1 +1,12 @@
-Write-Output "weebkl!"
+Write-Output "Script should take some time to run"
+
+Do {
+    $i=0
+    if($i -lt 10) {
+        Write-Output "Script running on"}
+        Start-Sleep -Seconds 5
+        $i++
+    else {    
+        Write-Output "Script has run for 50 seconds"
+    }
+} While ($i -lt 10)
