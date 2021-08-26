67259989396fc994d02922a3be7270edbadb1f11
Deborah Barnard <deborah@starfallprojects.co.uk>
2021-08-26 12:43:08 +0100

    test skip queued 1

--- script.ps1
+++ script.ps1
@@ -1,7 +1,8 @@
 Write-Output "Script should take some time to run"
+Write-Output "Small change"
 $i=0
 Do {    
-    if($i -lt 10) {
+    if($i -lt 9) {
         Write-Output "Script running on"
         Start-Sleep -Seconds 5
         $i++
